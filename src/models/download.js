291443aab45c31e5506c49e8be3c2d9d99065b2f
Chris Lander <chrislander@Chriss-MacBook-Pro.local>
2017-04-26 17:30:09 +0100

    committing new structure

--- src/models/download.js
+++ src/models/download.js
@@ -1,7 +1,51 @@
-var download = function(uri, filename, callback){
-    request.head(uri, function(err, res, body){               
+var path    = require('path'),
+    fs      = require('fs'),
+    request = require('request'),
+    mkdirp  = require('mkdirp');
+
+
+
+// Example call
+// download('https://www.google.com/images/srpr/logo3w.png', saveDir + '/google_' + model.colors[i] + '_' + frames[j] + '.png', function(){     
+// });          
+//
+
+exports.download_all = function (filename) {    
+    
+    var models  = JSON.parse(fs.readFileSync(filename, 'utf8')); 
+
+    for (var key in models){
+        var model = models[key],
+            dir = 'saved/'+model.audaID+'_'+model.doors+model.style,
+            filename = /google_' + model.colors[i] + '_' + frames[j] + '.png'
+            return console.log(dir);
+        for (var i in obj.urls){
+            
+        }
+    }
+    
+    this.makedir();
+    this.download();
+ 
+}
+
+exports.download_single = function (obj, audaID) {
+    return console.log('in download single');
+}
+
+exports.makedir = function (){
+    return console.log('in makedir');
+    mkdirp(saveDir, function (err) {
+        if (err)
+            console.error(err);
+    });    
+}
+
+exports.download = function (uri, filename, callback) {        
+    return console.log('in download');
+    request.head(uri, function (err, res, body) {
         console.log('content-type:', res.headers['content-type']);
         console.log('content-length:', res.headers['content-length']);
-        request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);               
+        request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
     });
-};
+};  
--- src/models/generate_urls.js
+++ src/models/generate_urls.js
@@ -1,20 +1,17 @@
-var createUrls = function (obj){
-    var dlcounter = 0;
-    var urlbase = "http://tuning-solera.herokuapp.com/",
-        frames = [0,1,2,3,4,5,6,7,"home"];
-        //We need just to off these frames
+var fs          = require('fs');
+    beautify    = require("json-beautify");
+
+exports.generate_urls = function (filename){
+    
+    var obj         = JSON.parse(fs.readFileSync(filename, 'utf8'));    
+        urlbase     = "http://tuning-solera.herokuapp.com/",
+        frames      = [0,1,2,3,4,5,6,7,"home"];
         
     for (var key in obj){
-         
+
         var model = obj[key];        
             model.urls = []; 
-              
-        var saveDir = baseDir + '/' + model.audaID + '_' + model.doors + model.style;
-        
-        mkdirp(saveDir, function(err) { 
-            if (err) console.error(err);            
-        });
-        
+                      
         for ( var i = 0; i < model.colors.length; i++ ){
             
             for ( var j = 0; j < frames.length; j++ ){   
@@ -22,15 +19,12 @@ var createUrls = function (obj){
                     color = getcolor[0], 
                     url = urlbase + model.audaID + '/' + model.doors + '/' + model.style + '/' + color + "/" + frames[j];
                                     
-                model.urls.push(url);
-                dlcounter++
-                
-                //download('https://www.google.com/images/srpr/logo3w.png', saveDir + '/google_' + model.colors[i] + '_' + frames[j] + '.png', function(){
-                //     
-                // });                                
+                model.urls.push(url);                
             }                
-        }
-        console.log(model.urls);
-        console.log(dlcounter);
-   
+        }                   
     }  
+    
+    fs.writeFile("data/urls.json", beautify(obj, null, 2, 100), function(err) {
+        if(err) return console.log(err);
+    }); 
+}
